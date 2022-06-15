961e3aa083fe3fdc460c68cda11babb65faf64b4
phinic925 <phinicnyangate480@gmail.com>
2022-06-15 10:33:27 +0100

    Done.

--- main.js
+++ main.js
@@ -3,6 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal = document.querySelector("#modal")
+errorModal.classList=('hidden')
+const like = querySelectorAll("like-glyph")
+function likeBack(e){
+  const heart = e.target;
+  mimicServerCall("url")
+  .then(resp=>{
+    if (heart.innerText=== EMPTY_HEART){
+      heart.innerText = FULL_HEART;
+      heart.className ="activated-heart";
+
+    }
+    else{
+      heart.innerText = EMPTY_HEART;
+      heart.className = "";
+    }
+  })
+  .catch(error=>{
+    const modal=document.getElementById('modal');
+    modal.className ="";
+    modal.innerHTML = error;
+    setTimeout(()=> modal.className="hiddden",3000);
+
+  });
+
+
+}
+for (const glyph of like){
+  glyph.addEventListener("click",likeBack)
+}
+
 
 
 
